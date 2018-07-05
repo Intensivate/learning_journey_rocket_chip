19e7d5aea6c27c12b3627cfced9346388ea76732
Aleksandar Pajkanovic <aleksandar.pajkanovic@gmail.com>
2018-07-05 23:52:39 +0200

    Add PROJ_NAME=F to set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -4,6 +4,7 @@ echo "........NOTE THIS SCRIPT MUST BE CALLED FROM THE CORRECT PATH........"
 echo
 
 unset VERILATOR_ROOT
+export PROJ_NAME=F
 export RISCV=$PWD/riscv-tools
 export MAKEFLAGS="$MAKEFLAGS -j1"
 export PATH=$RISCV/bin:$RISCV/../emulator/verilator/install/bin:$PATH
