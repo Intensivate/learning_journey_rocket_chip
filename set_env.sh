1328d51cf1a3568f0ccf6711a5ceb57df2085075
Aleksandar Pajkanovic <aleksandar.pajkanovic@gmail.com>
2018-07-05 15:43:59 +0200

    Adding set_env.sh

---
+++ set_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# source this file
+echo "........NOTE THIS SCRIPT MUST BE CALLED FROM THE CORRECT PATH........"
+echo
+
+unset VERILATOR_ROOT
+export RISCV=$PWD/riscv-tools
+export MAKEFLAGS="$MAKEFLAGS -j1"
+export PATH=$RISCV/bin:$RISCV/../emulator/verilator/install/bin:$PATH
+export VERILATOR_ROOT=$RISCV/../emulator/verilator/install/
